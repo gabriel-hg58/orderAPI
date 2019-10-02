a2c83f6e7aa7035ca498e680950659c074f08192
Gabriel Garcia <31714430+gabriel-hg58@users.noreply.github.com>
2019-10-02 14:26:19 -0300

    OrderRest and RabbitMQ
    Created OrderRest and
    introducing RabbitMQ

---
+++ src/main/java/com/orderAPI/controller/OrderRest.java
@@ -0,0 +1,48 @@
+package com.orderAPI.controller;
+
+import com.orderAPI.model.OrderData;
+import com.orderAPI.service.OrderDataService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/pedido")
+public class OrderRest {
+
+    @Autowired
+    private OrderDataService orderService;
+
+    @RequestMapping(value = "/lista", method = RequestMethod.GET)
+    public List<OrderData> orderList() {
+        return orderService.findAll();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Optional<OrderData> findOrder(@PathVariable int id) {
+        return orderService.findById(id);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String addOrder(@RequestBody OrderData order) {
+        try {
+            OrderData savedOrder = orderService.save(order);
+            return "Pedido salvo com sucesso!";
+        } catch (Exception e) {
+            return "Ocorreu um erro ao salvar o pedido";
+        }
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    public OrderData updateOrder(@RequestBody OrderData order) {
+        OrderData updatedOrder = orderService.save(order);
+        return updatedOrder;
+    }
+}
